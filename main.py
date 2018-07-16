b4bfb864fccf4f211e48c2ff7933ad1cc4b9dd4e
Michael Shepherd <mikesheep24@gmail.com>
2018-07-16 13:48:40 +0200

    Scrape and file creation completed

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 import requests
 import csv
+import sys
 import arrow
+import os
+import lxml.html
 from bs4 import BeautifulSoup
 
-# URL = 'http://t2000-05.sun.ac.za/EksamenUitslae/EksUitslae.jsp;jsessionid=92e880a930da9645a8f4d5c6429abd668c779bc863f4.e3iQbN0MahmLe3mNb3qTb34NaO0?pLang=1&TARGET=http%3A%2F%2Ft2000-05.sun.ac.za%2FEksamenUitslae%2FEksUitslae.jsp%3FpLang%3D1'
-URL = 'http://t2000-05.sun.ac.za/EksamenUitslae/EksUitslae.jsp?pLang=1'
-def get_soup(URL, username="19059019", password="Sheep19962"):
+URL = 'https://sso-prod.sun.ac.za/cas/login?TARGET=http%3A%2F%2Ft2000-05.sun.ac.za%2FEksamenUitslae%2FEksUitslae.jsp%3FpLang%3D1'
+DEBUG = 0
+
+def get_soup(URL, username, password, debug=0):
 	try:
-		post_values = {'username' : username, 'password' : password}
-		response = requests.post(URL, data=post_values)
-		content = response.content
-		return BeautifulSoup(content, "html.parser")
+		with requests.session() as session:
+			content = lxml.html.fromstring(session.get(URL).text)
+			hidden_elements = content.xpath('//form//input[@type="hidden"]')
+			post_values = {element.attrib['name']: element.attrib['value'] for element in hidden_elements}
+			post_values['username'] =  username
+			post_values['password'] =  password
+
+			content = session.post(URL, data=post_values)
+
+			if (content.url != URL):
+				return BeautifulSoup(content.content, "lxml")
+			else:
+				return("CONNECTION ERROR")
 	except:
 		return("CONNECTION ERROR")
 
-def save_marks(soup):
-	filename = arrow.now().format("DDMMYYHHmm") + ".csv"
+def save_marks(soup, debug=0):
+	filename = sys.argv[1]+ "_" + arrow.now().format("DDMMYYHHmm") + ".csv"
+	file = open(os.path.join("marks", filename), "w+")
+	csv = ""
+
 	table = soup.find_all("table")[1]
-	print(soup)
 	rows = table.find_all("tr")
 	del rows[0]
 
@@ -25,12 +40,25 @@ def save_marks(soup):
 		cols = row.find_all("td")
 		for col in cols:
 			if not col.find("img"):
-				print(col.text)
+				csv += col.text + ','
 			else:
-				print()
-		print()
+				csv += '-,'
+		csv += ",\n"
+
+	file.write(csv)
+
+	if debug == 1:
+		print(csv)
 
 
 if __name__ == "__main__":
-	soup = get_soup(URL)
-	save_marks(soup)
+	if len(sys.argv) < 3:
+		print("Usage: python3 main.py <username> <password> [DEBUG]")
+	else:
+		if len(sys.argv) > 3:
+			DEBUG = int(sys.argv[3])
+		soup = get_soup(URL, sys.argv[1], sys.argv[2], DEBUG)
+		if(soup != "CONNECTION ERROR"):
+			save_marks(soup, DEBUG)
+		else:
+			print(soup)
