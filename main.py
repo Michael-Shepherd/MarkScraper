af777f421549f680dac7e624d769878e8cf34b1c
Michael-Shepherd <sirsheepy@hotmail.com>
2018-07-28 14:27:55 +0200

    This is so broken that I dont even know why its broken.

--- main.py
+++ main.py
@@ -3,22 +3,28 @@ import csv
 import sys
 import arrow
 import os
+import time
 import lxml.html
 from bs4 import BeautifulSoup
 
-URL = 'https://sso-prod.sun.ac.za/cas/login?TARGET=http%3A%2F%2Ft2000-05.sun.ac.za%2FEksamenUitslae%2FEksUitslae.jsp%3FpLang%3D1'
+# URL = 'https://sso-prod.sun.ac.za/cas/login?TARGET=http%3A%2F%2Ft2000-05.sun.ac.za%2FEksamenUitslae%2FEksUitslae.jsp%3FpLang%3D1'
+URL = 'https://web-apps.sun.ac.za/AcademicResults/'
 DEBUG = 0
 
 def get_soup(URL, username, password, debug=0):
 	try:
 		with requests.session() as session:
+			time.sleep(0.01)
 			content = lxml.html.fromstring(session.get(URL).text)
 			hidden_elements = content.xpath('//form//input[@type="hidden"]')
 			post_values = {element.attrib['name']: element.attrib['value'] for element in hidden_elements}
 			post_values['username'] =  username
 			post_values['password'] =  password
+			print(post_values)
 
+			time.sleep(0.01)
 			content = session.post(URL, data=post_values)
+			print(content.content)
 
 			if (content.url != URL):
 				return BeautifulSoup(content.content, "lxml")
@@ -28,8 +34,7 @@ def get_soup(URL, username, password, debug=0):
 		return("CONNECTION ERROR")
 
 def save_marks(soup, debug=0):
-	if debug != 0:
-		print(soup)
+	print(soup)
 	filename = sys.argv[1]+ "_" + arrow.now().format("DDMMYYHHmm") + ".csv"
 	file = open(os.path.join("marks", filename), "w+")
 	csv = ""
